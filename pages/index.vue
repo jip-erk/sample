a814d8e460a2d4b34b35c5cdf1fb5f39a6628320
multityy <59746858+jip-erk@users.noreply.github.com>
2023-09-24 19:42:08 +0200

    init

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <audio controls>
+      <source
+        src="@/assets/sounds/1 - Reflection (Default Ringtone).wav"
+        type="audio/wav"
+      />
+    </audio>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
